a496db28d03b0da0d5c5f9992ba234467eca2ef1
mimigargon <mireiag77@gmail.com>
2023-06-06 11:00:24 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio-3_Numero-Par-o-Impar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_Numero_Par_o_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número entero:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("El número {0} es {1}", num, isPairOrNot(num));
+        }
+
+        public static string isPairOrNot(int num)
+        {
+            string result = "";
+            if(num % 2 == 0)
+            {
+                result = "par";
+            } else
+            {
+                result = "impar";
+            }
+
+            return result;
+        }
+    }
+}
